437d614d40a299800b75d5493dabc0ccd3ef35a7
Pták <ptakomuz@proton.me>
2025-08-15 19:53:24 +0200

    working prototype #2

--- main.py
+++ main.py
@@ -1,29 +1,21 @@
-import asyncio
-from playwright.async_api import async_playwright
-import time
+import requests as r
+from escape import escape
 
+URLNDK = "https://www.ndk.cz/"
 
-async def main():
-    async with async_playwright() as pw:
-        browser = await pw.chromium.launch(headless=False)
-        context = await browser.new_context(user_agent="Mozilla/5.0 (Windows NT  10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36");
-        page = await context.new_page()
-        await page.goto(
-            "https://kramerius.lib.cas.cz/view/uuid:cac19033-48e1-11e1-1154-001143e3f55c?page=uuid:cac19034-48e1-11e1-1154-001143e3f55c"
-        )
-        await page.wait_for_timeout(7000)
-        await page.mouse.wheel(10, 30);
-        textButton = await page.query_selector_all('//button[@class="mat-focus-indicator mat-tooltip-trigger mat-icon-button mat-button-base ng-star-inserted"]')
-        await textButton[3].click()
-        await page.wait_for_timeout(5000)
+def main(root_pid):
+    session = r.Session()
+    response = session.get(URLNDK + "search/api/v5.0/search?q=root_pid:" + escape(root_pid)+" AND document_type:page&fl=PID&rows=9999999&wt=json", stream=True)
+    text = ""
+    for PIDobject in response.json().get("response").get("docs"):
+        PID = PIDobject.get("PID")
+        print(PID)
+        page = session.get(f"https://ndk.cz/search/api/v5.0/item/{PID}/streams/TEXT_OCR")
+        text = text + page.text
 
-        popup = await page.query_selector_all(
-            '//div[@class="app-text-content"]'
-        )
-        text = await popup[0].text_content()
-        print(text)
-        await browser.close()
+    with open ("output.txt", "w") as file:
+        file.write(text)
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    main("uuid:a837aec0-8c99-11de-9ad9-000d606f5dc6")
